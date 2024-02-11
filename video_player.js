3c7861e818b5d9c26081f8efef956e8fa42a6ba8
Prashant528 <prashant.tandan528@gmail.com>
2024-02-10 21:07:31 -0800

    Added a dummy video player.Nodes are clickable

---
+++ video_player.js
@@ -0,0 +1,10 @@
+// Function to open the popup
+function openPopup() {
+    document.getElementById("popupOverlay").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+}
+
